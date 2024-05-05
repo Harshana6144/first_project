8127fa6dcf516866aa443d0031a6fc649481332a
Harshana Prasad <harshana7096@gmail.com>
2024-05-05 23:27:01 +0530

    created Scaffold for flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+/*class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home:Text('Hello World'),
+     // home:Text('Helow Harshana'),
+    );
+  }
+}*/
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title:"new app",
+      home: Scaffold(),
     );
   }
 }
-
